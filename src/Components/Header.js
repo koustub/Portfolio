28db48ee38f86ce3384d2cad569428ed58f24b95
koustub manchiraju <koustub.247@gmail.com>
2020-06-01 14:27:22 -0400

    intital commit

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+function Header() {
+    return (
+        <>
+            <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                    <h1 class="display-4">KOUSTUB MANCHIRAJU</h1>
+                    <p class="lead">I 'M FULL STACK WEB DEVELOPER </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Header;
